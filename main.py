53b2f45c359d5c91b41bde949dfd1692b5ed0c69
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-07-06 13:04:47 +0500

    CRUD

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 def load_users():
@@ -16,7 +16,6 @@ def save_user(data):
 app = FastAPI()
 
 class User(BaseModel):
-
     id: Annotated[str, Field(..., description='ID of the user', examples=['P001'])]
     first_name: Annotated[str, Field(..., description='First name of the user')]
     last_name: Annotated[str, Field(..., description='Last name of the user')]
@@ -27,6 +26,12 @@ class User(BaseModel):
     @property
     def full_name(self) -> str:
         return self.first_name + " " + self.last_name
+
+class UserUpdate(BaseModel):
+    first_name: Annotated[Optional[str], Field(default=None)]
+    last_name: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
 
 
 @app.get("/")
@@ -48,3 +53,48 @@ def create_user(user: User):
 
     return JSONResponse(status_code=201, content={'message':'User created successfully'})
 
+@app.put('/edit/{id}')
+def update_user(id: str, user_update: UserUpdate):
+
+    data = load_users()
+
+    if id not in data:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    # Extract user to update
+    existing_user = data[id]
+
+    # Convert pydantic obj into python dict and allow only the set (edited) fields
+    updated_user = user_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_user.items():
+        existing_user[key] = value
+
+    existing_user['id'] = id
+
+    # Convert the python dict again into pydantic obj to let the fields ("full_name") recalculated
+    user_pydandic_obj = User(**existing_user)
+
+    # Convert pydantic obj back into python dict and exclude "id"
+    existing_user = user_pydandic_obj.model_dump(exclude={'id'})
+
+    # Add updated dict into users data
+    data[id] = existing_user
+
+    save_user(data)
+
+    return JSONResponse(status_code=200, content={'message':'User updated'})
+
+@app.delete('/delete/{id}')
+def delete_user(id: str):
+
+    data = load_users()
+
+    if id not in data:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del data[id]
+
+    save_user(data)
+
+    return JSONResponse(status_code=200, content={'message':'User deleted'})
